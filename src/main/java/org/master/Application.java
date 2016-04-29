fb69191793bb7ab5444cddcacf1be2faf2003681
kaenry <kaenry@163.com>
2016-04-29 12:10:39 +0800

    first init commit

---
+++ src/main/java/org/master/Application.java
@@ -0,0 +1,16 @@
+package org.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zouwen on 2016/4/29.
+ * Application BootStartup
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
